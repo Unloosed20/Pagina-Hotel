82ecb05c3243a9f5b1d1d75bdf78d5a88877d32c
Unloosed <reyjosue543@gmail.com>
2025-03-30 14:23:41 -0600

    Fix production build

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Importante para despliegue con HashRouter
+  base: './',
   build: {
-    outDir: "dist", // Asegura que el directorio de salida sea correcto
-  },
-  server: {
-    historyApiFallback: true, // Redirige todas las rutas al index.html en desarrollo
+    outDir: "dist"
   }
 })
+
 
