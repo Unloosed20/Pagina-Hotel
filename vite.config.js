6620af1770dec89ed2b1027d66ae52849275157f
Unloosed <reyjosue543@gmail.com>
2025-03-30 12:53:59 -0600

    Actualización de rutas con HashRouter

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Asegura que las rutas sean relativas a la raíz
   build: {
     outDir: "dist", // Asegura que el directorio de salida sea correcto
   },
@@ -12,3 +11,4 @@ export default defineConfig({
     historyApiFallback: true, // Redirige todas las rutas al index.html en desarrollo
   }
 })
+
