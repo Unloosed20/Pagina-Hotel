479844b79431f304f694411c9c85e0f915e0de93
Unloosed <reyjosue543@gmail.com>
2025-05-11 19:05:14 -0600

    Actualizacion

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,10 +2,8 @@
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = "https://ybvessbqodzbsvevzglc.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlidmVzc2Jxb2Rlc2Jxb2R6YnN2ZXZ6Z2xjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMzODE2NzIsImV4cCI6MjA1ODk1NzY3Mn0.gbvCnLUJJkTbNXZCJsdqhQfPJtL1m7fwuZwHP0bg_cw"; // ⚠️ Reemplaza con tu clave
-
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlidmVzc2Jxb2R6YnN2ZXZ6Z2xjIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0MzM4MTY3MiwiZXhwIjoyMDU4OTU3NjcyfQ.nIN1h4_wI4rA2m8way0NzuncyYdpvNOMoupiSRr4YWI"; // ⚠️ Reemplaza con tu clave
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-
 async function ensureBuckets() {
   const buckets = [
     { name: "habitaciones", public: true },
