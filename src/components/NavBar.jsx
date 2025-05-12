b637e87a03894445135d43c5f37e61467e8bd49b
Unloosed <reyjosue543@gmail.com>
2025-05-12 13:17:33 -0600

    Reserva Habitacion

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
         <div className="nav-buttons">
           <Link to="/" className="cta-button">Iniciar sesion</Link>
           <Link to="/registro-clientes" className="cta-button">Registrarse</Link>
-          <Link to="/habitaciones-clientes" className="cta-button">Habitaciones</Link>
+          <Link to="/habitaciones-cliente" className="cta-button">Habitaciones</Link>
         </div>
       </div>
     </header>
