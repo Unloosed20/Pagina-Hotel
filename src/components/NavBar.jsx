5e8634b0704782a8dc97db0b534823b63134bdef
Unloosed <reyjosue543@gmail.com>
2025-05-25 13:41:17 -0600

    Gestion Servicios

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,11 +7,13 @@ const Navbar = () => {
       <div className="header-container">
         <h1 className="hotel-name">Hotel Punta Arena</h1>
         <div className="nav-buttons">
-          <Link to="/" className="cta-button">Iniciar sesion</Link>
-          <Link to="/registro-clientes" className="cta-button">Registrarse</Link>
+          <Link to="/pagina-principal" className="cta-button">Pagina Principal</Link>
           <Link to="/habitaciones-cliente" className="cta-button">Habitaciones</Link>
           <Link to="/restaurante-cliente" className="cta-button">Restaurante</Link>
           <Link to="/servicio-cliente" className="cta-button">Servicios</Link>
+          <Link to="/perfil-cliente" className="cta-button">Perfil</Link>
+          <Link to="/" className="cta-button">Iniciar sesion</Link>
+          <Link to="/registro-clientes" className="cta-button">Registrarse</Link>
           <Link to="/" className="cta-button">Cerrar sesion</Link>
         </div>
       </div>
